d8937cf60f5cafd7d6e93fb4231f2d83bc9b1a30
Milenko Bugueno <milenko@milenkos-air.home>
2014-01-30 17:44:59 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileMovesOs::Application.config.secret_token = '8ba90d51d3ee7205943aba960434bb5ec9f33f88d6293e09227ab06439f033cd5def9c5d14e0dc741d947a14afd76ba03c679a6556d3d68ce097e16e23966d49'
