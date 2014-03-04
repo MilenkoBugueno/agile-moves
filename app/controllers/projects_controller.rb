41933072fa266a74a765b670f4dbe12de47adbb1
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-03-04 05:58:39 +0100

    adding header and report view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,6 +52,8 @@ class ProjectsController < ApplicationController
   end
   
   def report
+    @project = Project.find(params[:id])
+    
     @tomatoes = Tomato.order('state DESC')
     @tomatoes = @tomatoes.by_user_id(params[:user]) if params[:user].present?
     @tomatoes_by_date = @tomatoes.group_by(&:publish_date)
