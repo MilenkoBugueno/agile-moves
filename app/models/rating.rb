39a20f367b251926ae788152a20f460d4b4668b6
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-03-16 13:16:41 +0100

    validating presence of star_rating and thumb_rating
    fixing issue #14

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,19 @@
 class Rating < ActiveRecord::Base
   attr_accessible :body, :star_rating, :thumb_rating, :user_id, :move_id
+  validates_presence_of :star_rating, :thumb_rating
+  
   belongs_to :move
   belongs_to :user
+  
+  after_initialize :init
+ 
+  private
+    def init
+      if self.new_record? && self.star_rating.nil?
+        self.star_rating = -1
+      end
+      if self.new_record? && self.thumb_rating.nil?
+        self.thumb_rating = -1
+      end
+    end
 end
