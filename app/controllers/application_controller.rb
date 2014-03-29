a73aeac5d98e86c7adc17cef57450787aa4a0bf1
myronymus <myronymus@gmail.com>
2014-03-29 18:09:52 +0100

    File für die Deutschen Texte

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+#To set the locale for more than one request.
+  before_filter :set_locale
+  private
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+#To prevent routing Error, when setting locale
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
 end
