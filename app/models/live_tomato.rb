cd31d1c0efccc260258931c0ac61c0e22e234f20
tgruendl <tgruendl@gmx.de>
2014-04-24 20:24:33 +0200

    Verbleibende Tomatenzeit eingecheckt

--- app/models/live_tomato.rb
+++ app/models/live_tomato.rb
@@ -1,4 +1,24 @@
 class LiveTomato < ActiveRecord::Base
   attr_accessible :currenttime, :endtime, :starttime, :title, :username, :userid, :status, :tomato_id, :star_rating, :thumb_rating
   belongs_to :tomato
+
+  def remaining_time(id)
+    tomato = LiveTomato.find(id)
+
+    endtime = Time.parse(tomato.endtime)
+    now = Time.now
+
+    remaining = ((endtime - now) / 60) + 1
+
+    if remaining < 0
+      remaining = 0
+    else
+      remaining.to_i
+    end
+    if tomato.status == 2
+      remaining = 0
+    else
+      remaining.to_i
+    end
+  end
 end
