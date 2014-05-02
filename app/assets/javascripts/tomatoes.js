8d07f1a511695ebb364912114cfc0deb563c7e36
tgruendl <tgruendl@gmx.de>
2014-05-02 02:35:41 +0200

    Browserleistencountdown und Ton beim Beenden der Tomate

--- app/assets/javascripts/tomatoes.js
+++ app/assets/javascripts/tomatoes.js
@@ -1,3 +1,5 @@
+var tt_tomaten_zeit;
+
 function start_me(userid, username, title, tomato_id) {
 
     var anfang;
@@ -13,6 +15,7 @@ function start_me(userid, username, title, tomato_id) {
     var fnull;
     var ezeit;
     var tomatendauer;
+    var titletimer;
 
     x = document.getElementById("btoStart").name;
     tomatendauer = document.getElementById("txtTimer").value;
@@ -23,6 +26,7 @@ function start_me(userid, username, title, tomato_id) {
     fnull = ((stime < 10) ? "0" : "");
     stime = fnull + tomatendauer + ":00";
     //stime = "00:05";   //Debug-Zeit
+    tt_tomaten_zeit = stime;
     azeit = stunden + anfang.getHours() + ":" + minuten + anfang.getMinutes();
     ezeit = estunden + ende.getHours() + ":" + eminuten + ende.getMinutes();
     window.open('/de/live_tomatoes/new?live=1&tomato_id=' + tomato_id + '&userid=' + userid + '&username=' + username + '&title=' + title + '&starttime=' + azeit + '&endtime=' + ezeit + '&currenttime=' + tomatendauer,'mywindow');
@@ -31,11 +35,12 @@ function start_me(userid, username, title, tomato_id) {
     {
 
         document.getElementById("counter_2").innerHTML = ""; //entfernt die Timer-Grafik aus dem div
+        titletimer = setInterval("update_title()", 1000);
         $(function(){
             $('#counter_2').countdown({
             image: '/assets/digits.png',
             startTime: stime,
-            timerEnd: function(){ end_me(); },
+            timerEnd: function(){ end_me(titletimer); },
             format: 'mm:ss'
             });
         });
@@ -49,7 +54,7 @@ function start_me(userid, username, title, tomato_id) {
     }
 }
 
-function end_me() {
+function end_me(titletimer) {
 
     var ende;
     var ezeit;
@@ -64,9 +69,39 @@ function end_me() {
 
     document.getElementById("btoStart").name = "los";
     document.getElementById("btoStart").disabled = false;
-
+    window.clearInterval(titletimer);
+    document.getElementById("tomato_finished_id").play();
     alert("AUFHÖREN!");
 
+}
+
+function update_title() {
+    var zeit = tt_tomaten_zeit.split(":");
+    var tmp;
+    var anzeigezeit;
+
+    if (tt_tomaten_zeit != "0:00") {
+        switch (Number(zeit[1])) {
+            case 0:
+                zeit[1] = "59";
+                if (Number(zeit[1]) > 0) {
+                    tmp = Number(zeit[0]) - 1;
+                    zeit[0] = tmp.toString();
+                }
+                break;
+            case 1:
+                zeit[1] = "00";
+                break;
+            default:
+                tmp = Number(zeit[1]) - 1;
+                zeit[1] = tmp.toString();
+        }
+    }
+    var sekunden  = ((zeit[1].length == 1) ? "0" : "");
+    var minuten  = ((zeit[0].length == 1) ? "0" : "");
+    tt_tomaten_zeit = zeit[0] + ":" + zeit[1];
+    anzeigezeit = minuten + zeit[0] + ":" + sekunden + zeit[1];
+    window.document.title = anzeigezeit;
 }
 
 function rate_me(x) {
