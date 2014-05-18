3bb778928947e456c070cfb8d04fccd102252f06
tgruendl <tgruendl@gmx.de>
2014-05-19 00:38:06 +0200

    First Version of Wordpress Publishing built in. No error handling yet.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,77 @@ class ApplicationController < ActionController::Base
     @activity.params = params.inspect
     @activity.save
   end
+
+
+  def wp_publish(host, user, pwd, content, title, categories)
+
+    wp = Rubypress::Client.new(:host => host, :username => user, :password => pwd)
+
+    articleid = wp.newPost(:content => {:post_status => "publish", :post_date => Time.now, :post_content => content, :post_title => title, :terms_names => {:category => categories}})
+
+    # Mark for garbage collection
+    wp = nil
+
+    # TODO Errorhandling
+  end
+
+  def wp_getCategories(host, user, pwd, blog_id)
+
+    wp = Rubypress::Client.new(:host => host, :username => user, :password => pwd)
+
+    blog_tax = wp.getTerms(:blogid => blog_id, :username => user, :password => pwd, :taxonomy => "category")
+
+    categories = Array.new
+    blog_tax.each do |cat|
+      categories = categories.push(cat["name"])
+    end
+    wp = nil
+    return categories
+  end
+
+  def wp_getUsersBlogs(host, user, pwd)
+
+    wp = Rubypress::Client.new(:host => host, :username => user, :password => pwd)
+
+    user_blogs = wp.getUsersBlogs()
+
+    blogs = Array.new
+    user_blogs.each do |blog|
+      blogs = blogs.push(blog["url"].sub(/^http\:\/\//, '').delete('/'))
+    end
+    wp = nil
+    return blogs
+  end
+
+  def wp_pre_publish(blogstringarray)
+
+    i = 0
+    # Dieses Array ist für die Url [0] und die Kategorie [1] aus dem blogstringarray
+    blogsarray = Array.new
+
+    # Dieser Hash soll die url als key haben und die Kategorien als Array zum Key
+    bloghash = Hash.new
+    pre_blog = nil
+
+    blogstringarray.each do |blog|
+      blogsarray = Array.new
+      blogsarray = blog.split("!")
+      if i == 0
+        bloghash[blogsarray[0]] = Array.new
+        bloghash[blogsarray[0]].push(blogsarray[1])
+        pre_blog = blogsarray[0]
+      else
+        if pre_blog == blogsarray[0]
+          bloghash[blogsarray[0]].push(blogsarray[1])
+          pre_blog = blogsarray[0]
+        else
+          bloghash[blogsarray[0]] = Array.new
+          bloghash[blogsarray[0]].push(blogsarray[1])
+          pre_blog = blogsarray[0]
+        end
+      end
+      i = i + 1
+    end
+    return bloghash
+  end
 end
