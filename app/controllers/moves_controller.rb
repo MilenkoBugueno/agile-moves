70c414b56da3ecd82523eff8d83fdd8322aa50d9
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-06-06 16:16:27 +0200

    Adding widget feature to the views

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -39,11 +39,11 @@ class MovesController < ApplicationController
     end
 
     #@wp_categories = wp_getCategories("learningtocode.de", "teamtool", "teamtool01", "1")
-    @wp_categories = Hash.new
-    @wp_blogs = wp_getUsersBlogs("learningtocode.de", "teamtool", "teamtool01")
-    @wp_blogs.each do |blog|
-      @wp_categories[blog] = wp_getCategories(blog, "teamtool", "teamtool01", "1")
-    end
+    #@wp_categories = Hash.new
+    #@wp_blogs = wp_getUsersBlogs("learningtocode.de", "teamtool", "teamtool01")
+    #@wp_blogs.each do |blog|
+    #  @wp_categories[blog] = wp_getCategories(blog, "teamtool", "teamtool01", "1")
+    #end
 
     # Wordpress publishing
     if params[:wppublish] == "true"
