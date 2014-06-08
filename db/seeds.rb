d87414e7e536076065aa31979ec836ec47b2d711
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-06-08 12:03:13 +0200

    Adding Widgets

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,12 +26,44 @@ puts 'user: ' << user1.name
 user2 = User.find_or_create_by_email :name => "user2", :email => "user2@example.com", :password => "changeme"
 puts 'user: ' << user2.name
 
+
+puts 'DEFAULT WIDGETS'
+work_rating = Widget.find_or_create_by_title(:title => "Work rating", :path=>"projects/work_rating", :widget_type => 1)
+
+work_tomatoes = Widget.find_or_create_by_title(:title => "Work tomatoes", :path=>"projects/work_tomatoes", :widget_type => 1)
+
+
+report_rating = Widget.find_or_create_by_title(:title => "Report rating", :path=>"projects/report_rating", :widget_type => 2)
+
+report_tomatoes = Widget.find_or_create_by_title(:title => "Report tomatoes", :path=>"projects/report_tomatoes", :widget_type => 2)
+
+
+move_general = Widget.find_or_create_by_title(:title => "Move general", :path=>"moves/move_general", :widget_type => 3)
+
+moves_ratings = Widget.find_or_create_by_title(:title => "Moves ratings", :path=>"moves/ratings", :widget_type => 3)
+
+tomatoes_timer = Widget.find_or_create_by_title(:title => "Tomatoes timer", :path=>"moves/timer", :widget_type => 3)
+
+tomato_rating = Widget.find_or_create_by_title(:title => "Tomato rating", :path=>"moves/tomato_rating", :widget_type => 3)
+
+tomato_comments = Widget.find_or_create_by_title(:title => "Tomato comment", :path=>"shared/comments_list", :widget_type => 3)
+
+moves_actions = Widget.find_or_create_by_title(:title=> "Moves actions", :path=>"moves/actions", :widget_type => 3)
+
+publish_wordpress = Widget.find_or_create_by_title(:title => "Publish wordpress", :path=>"moves/publish_wordpress", :widget_type => 3)
+
+
 puts 'DEFAULT MOVE TYPES'
-star_move = MoveType.find_or_create_by_title :title => "star move", :star_rating => true, :thumb_rating => false, :has_tomatoes => false
+star_move = MoveType.find_or_create_by_title :title => "star move"
+star_move.widgets = [work_rating, report_rating, move_general, moves_ratings, moves_actions]
 puts 'move type: ' << star_move.title
-thumbs_move = MoveType.find_or_create_by_title :title => "thumb move", :thumb_rating => true, :star_rating => false, :has_tomatoes => false
+
+thumbs_move = MoveType.find_or_create_by_title :title => "thumb move"
+thumbs_move.widgets = [work_rating, report_rating, move_general, moves_ratings, moves_actions]
 puts 'move type: ' << thumbs_move.title
-tomatoes_move = MoveType.find_or_create_by_title :title => "tomato move", :thumb_rating => false, :star_rating => false, :has_tomatoes => true, :tomatoes_number => 1
+
+tomatoes_move = MoveType.find_or_create_by_title :title => "tomato move"
+tomatoes_move.widgets = [work_tomatoes, report_tomatoes, move_general, tomatoes_timer, tomato_rating, tomato_comments, moves_actions]
 puts 'move type: ' << tomatoes_move.title
 
 
@@ -45,3 +77,4 @@ project2 = Project.find_or_create_by_title(:title => "Test project 2")
 project2.users = [user, user2]
 project2.move_types = [thumbs_move, tomatoes_move]
 puts 'project: ' << project2.title
+
