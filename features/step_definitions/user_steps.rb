5957b9689432800e176db7073cdfda38020de613
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-07-08 21:33:25 +0200

    Fixing cucumber tests Adding new tomato icons

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -139,7 +139,7 @@ When /^I sign in with a wrong password$/ do
 end
 
 When /^I edit my account details$/ do
-  click_link "Edit account"
+  click_link "Edit Account"
   fill_in "Name", :with => "newname"
   fill_in "user_current_password", :with => @visitor[:password]
   click_button "Update"
