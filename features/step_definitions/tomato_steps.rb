3e413ad51ca857ef54ccbdeb8a596d8b05cf26ae
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-07-19 10:18:27 +0200

    New scenarios for connecting tomato moves

--- features/step_definitions/tomato_steps.rb
+++ features/step_definitions/tomato_steps.rb
@@ -38,6 +38,14 @@ When(/^I create (.*) new tomatoes for tomorrow$/) do |count|
   end
 end
 
+When(/^I have a move with (.*) tomato planned for today$/) do |arg1|
+  move_type = MoveType.find_or_create_by_title(:title => "Tomato")
+  create_move(move_type, Date.today, 1)
+end
+
+Given(/^I have several moves with (\d+) unplanned tomatoes$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
 
 ### THEN ###
 
@@ -45,10 +53,11 @@ Then(/^I see the message (.*)$/) do |message|
   page.should have_content message
 end
 
-And(/^I see (\d+) tomatoes in the todo today list$/) do |count|
+Then(/^I see (\d+) tomatoes in the todo today list$/) do |count|
   page.should have_content "Tomato"
 end
 
-And(/^I see (\d+) tomatoes in the activity inventory list$/) do |count|
+Then(/^I see (\d+) tomatoes in the activity inventory list$/) do |count|
   page.should have_content "Tomato"
 end
+
--- features/tomatoes/plan_tomatoes.feature
+++ features/tomatoes/plan_tomatoes.feature
@@ -33,3 +33,21 @@ Feature: Plan tomatoes
     When I create 3 new tomatoes
     Then I see the message Tomato was successfully created
     And I see 3 tomatoes in the activity inventory list
+
+  Scenario: Plan one tomato from any move
+    Given I am logged in
+    And I have a move with 1 tomato planned for today
+    When I am in the plan view
+    Then I see 1 tomatoes in the todo today list
+
+  Scenario: Plan one tomato from any move
+    Given I am logged in
+    And I have a move with 2 tomato planned for today
+    When I am in the plan view
+    Then I see 2 tomatoes in the todo today list
+
+  Scenario: Plan several tomatoes from several moves
+    Given I am logged in
+    And I have several moves with 7 unplanned tomatoes
+    When I am in the plan view
+    Then I see 7 tomatoes in the activity inventory list
