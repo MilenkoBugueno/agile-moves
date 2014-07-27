8f1b243f0bc9639e6839ca1b489c61f16cd93fbb
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-07-27 13:00:49 +0200

    Adding the icons for the team tomatoes

--- app/helpers/tomatoes_helper.rb
+++ app/helpers/tomatoes_helper.rb
@@ -1,15 +1,30 @@
 module TomatoesHelper
-  def tomato_icon_path(state)
-    if state == 1 #planed
-      path = "tomategruen.png"
-    elsif state == 2 #done
-      path = "tomaterot.png"
-    elsif state == 3 #extra unplanned
-      path = "tomateflieder.png"
-    elsif state == 4 #extra done
-      path = "tomatedunkellila.png"
+  def tomato_icon_path(tomato)
+    state = tomato.state
+    if tomato.users.count > 1
+      if state == 1 #planed
+        path = "teamtomatengrau.png"
+      elsif state == 2 #done
+        path = "teamtomatenrot.png"
+      elsif state == 3 #extra unplanned
+        path = "teamtomatenflieder.png"
+      elsif state == 4 #extra done
+        path = "teamtomatendunkellila.png"
+      else
+        path = "teamtomatengrau.png"
+      end
     else
-      path = "tomategrau.png"
+      if state == 1 #planed
+        path = "tomategruen.png"
+      elsif state == 2 #done
+        path = "tomaterot.png"
+      elsif state == 3 #extra unplanned
+        path = "tomateflieder.png"
+      elsif state == 4 #extra done
+        path = "tomatedunkellila.png"
+      else
+        path = "tomategrau.png"
+      end
     end
 
     return path
