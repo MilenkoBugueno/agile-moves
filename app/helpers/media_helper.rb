d6d7b30df1a876334c9146a1a1d3fff68fc78725
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-08-24 22:43:38 +0200

    Creating signature

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -1,2 +1,9 @@
 module MediaHelper
+  def signature(medium)
+    if medium.title.present? && medium.composer.present? && medium.catalog.present?
+      signature = (medium.composer[0..3].upcase() + medium.title[0..3].downcase() + medium.catalog).gsub(/\s+/, "")
+    end
+
+    return signature
+  end
 end
