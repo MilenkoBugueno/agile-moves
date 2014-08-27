2b2b2a49c64773b44fdce2cc2281ac183f81cf5b
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-08-27 23:14:55 +0200

    Creating signature

--- app/helpers/tomatoes_helper.rb
+++ app/helpers/tomatoes_helper.rb
@@ -30,4 +30,61 @@ module TomatoesHelper
     return path
   end
 
+  def tomato_icon_path_at_move(tomato, move)
+    state = tomato.state
+    if tomato.move.present? && move != tomato.move && tomato.move.planned_at.present? && move.planned_at.present?
+      if tomato.move.planned_at < move.planned_at
+        extra_planned = true
+      else
+        extra_planned = false
+      end
+    elsif state == 3 || state == 4
+      extra_planned = true
+    else
+      extra_planned = false
+    end
+
+    if tomato.users.count > 1
+      if !extra_planned
+        if state == 1 #planed
+          path = "teamtomatengrau.png"
+        elsif state == 2 #done
+          path = "teamtomatenrot.png"
+        else
+          path = "teamtomatengrau.png"
+        end
+      else
+        if state == 1 || state == 3 #extra unplanned
+          path = "teamtomatenflieder.png"
+        elsif state == 4 || state == 2 #extra done
+          path = "teamtomatendunkellila.png"
+        else
+          path = "teamtomatengrau.png"
+        end
+      end
+
+    else
+      if !extra_planned
+        if state == 1 #planed
+          path = "tomategrau.png"
+        elsif state == 2 #done
+          path = "tomaterot.png"
+        else
+          path = "tomategrau.png"
+        end
+      else
+        if state == 1 || state == 3 #extra unplanned
+          path = "tomateflieder.png"
+        elsif state == 4 || state == 2 #extra done
+          path = "tomatedunkellila.png"
+        else
+          path = "tomategrau.png"
+        end
+
+      end
+    end
+
+    return path
+  end
+
 end
