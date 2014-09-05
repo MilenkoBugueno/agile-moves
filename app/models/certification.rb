594e4445275e8ce7baa4a40f1d6229c809062a5a
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-09-05 12:37:14 +0200

    Making Certification prettier

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,7 +1,8 @@
 class Certification < ActiveRecord::Base
   attr_accessible :approvements, :description, :label, :level_id, :move_type_id, :moves_number, :title, :user_id
-
   #level_id: 1=apprenctive; 2=journeyman; 3=master;
+
+  validates_presence_of :title, :label, :move_type_id
 
   belongs_to :user
   belongs_to :move_type
