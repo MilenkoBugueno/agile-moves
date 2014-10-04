210c960a60a19dd49cedb6ea72e734c63a6f370f
MilenkoBugueno <milenko.bugueno@gmail.com>
2014-10-04 15:06:37 +0200

    Bugfix Work and plan view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,18 +11,4 @@ module ProjectsHelper
 
     return has_plan_view
   end
-
-  def has_certificate_view(project)
-    has_cert_view = false
-    project.move_types.each do |move_type|
-      move_type.widgets.each do |widget|
-        if widget.widget_type == 0
-          has_cert_view = true
-        end
-      end
-    end
-
-    return has_cert_view
-  end
-
 end
